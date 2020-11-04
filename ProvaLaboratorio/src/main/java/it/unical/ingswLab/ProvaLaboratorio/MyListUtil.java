9df03427f9171817fc037f72beb0747575608a65
ntl95 <fexd27@gmail.com>
2020-11-04 11:31:32 +0100

    First commit without test

---
+++ ProvaLaboratorio/src/main/java/it/unical/ingswLab/ProvaLaboratorio/MyListUtil.java
@@ -0,0 +1,22 @@
+package it.unical.ingswLab.ProvaLaboratorio;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MyListUtil {
+	
+	public List<Integer> sortList (List<Integer> input, String ordinamento){
+		
+		if(ordinamento == "crescente") {
+			Collections.sort(input);		
+		}
+		else {
+			Collections.sort(input);
+			Collections.reverse(input);
+		}
+		
+		return input;
+	}
+
+}
